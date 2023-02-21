282dcaea4cbc1f93aaa221987de6805a66f7eb48
chan <vsvx13@naver.com>
2023-02-21 18:36:03 +0900

    파이어베이스 인증을 이용한 이메일 로그인 및 회원가입 기능 구현,예외처리는 email과 password 둘 중 하나라도 비어있을 경우에만 처리. 파이어베이스에 저장된 이메일과 비밀번호를 입력 시 MainActivity로 이동

--- chatapp/app/src/main/java/com/example/chatapp/MainActivity.kt
+++ chatapp/app/src/main/java/com/example/chatapp/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.chatapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val currentUser = Firebase.auth.currentUser
+
+        if(currentUser == null){
+            //로그인이 안되어 있음, 로그인 액티비티를 실행한다.
+            startActivity(Intent(this, LoginActivity::class.java))
+            finish()
+        }
     }
 }
