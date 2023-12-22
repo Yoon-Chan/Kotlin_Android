a96b5682fae2beaebf0a81896cc79cf95bf908bc
윤찬 <vsvx13@naver.com>
2023-12-22 15:01:06 +0900

    Feat: 이름, 생년월일, 휴대폰 번호 무결성 체크

--- SecurityApp/app/src/main/java/com/example/securityapp/IdentityInputActivity.kt
+++ SecurityApp/app/src/main/java/com/example/securityapp/IdentityInputActivity.kt
@@ -28,44 +28,115 @@ class IdentityInputActivity : AppCompatActivity() {
     private fun initView() {
         with(binding) {
             nameEdit.setOnEditorActionListener(EditorInfo.IME_ACTION_NEXT) {
-                birthdayLayout.isVisible = true
-                birthdayEdit.showKeyboard()
-            }
-
-            birthdayEdit.doAfterTextChanged {
-                if(birthdayEdit.length() > 7){
-                    genderLayout.isVisible = true
-                    birthdayEdit.hideKeyboard()
+                if(validName()){
+                    nameLayout.error = null
+                    if(phoneLayout.isVisible){
+                        confirmButton.isVisible = true
+                    }else {
+                        birthdayLayout.isVisible = true
+                        birthdayEdit.showKeyboard()
+                    }
+                }else {
+                    confirmButton.isVisible = false
+                    nameLayout.error = "1자 이상의 한글을 입력해주세요."
                 }
             }
 
+            birthdayEdit.doAfterTextChanged {
+                if (birthdayEdit.length() > 7) {
+                    if (validBirthday()) {
+                        birthdayLayout.error = null
+                        if (phoneLayout.isVisible) {
+                            confirmButton.isVisible = true
+                        } else {
+                            genderLayout.isVisible = true
+                            birthdayEdit.hideKeyboard()
+                        }
+                    } else {
+                        confirmButton.isVisible = false
+                        birthdayLayout.error = "생년월일 형식이 다릅니다."
+                    }
+                }
+            }
+
+            birthdayEdit.setOnEditorActionListener(EditorInfo.IME_ACTION_DONE) {
+                val isValid = validBirthday() && birthdayEdit.length() > 7
+                if (isValid) {
+                    confirmButton.isVisible = phoneLayout.isVisible
+                    birthdayLayout.error = null
+                } else {
+                    birthdayLayout.error = "생년월일 형식이 다릅니다."
+                }
+                birthdayEdit.hideKeyboard()
+            }
+
             genderChipGroup.setOnCheckedStateChangeListener { group, checkedIds ->
-                if(!telecomLayout.isVisible){
+                if (!telecomLayout.isVisible) {
                     telecomLayout.isVisible = true
                 }
             }
 
             telecomChipGroup.setOnCheckedStateChangeListener { group, checkedIds ->
-                if(!phoneLayout.isVisible){
+                if (!phoneLayout.isVisible) {
                     phoneLayout.isVisible = true
                     phoneEdit.showKeyboard()
                 }
             }
 
             phoneEdit.doAfterTextChanged {
-                if(phoneEdit.length() > 10){
-                    confirmButton.isVisible = true
-                    phoneEdit.hideKeyboard()
+                if (phoneEdit.length() > 10) {
+                    if (validPhone()) {
+                        phoneLayout.error = null
+                        confirmButton.isVisible = true
+                        phoneEdit.hideKeyboard()
+                    } else {
+                        confirmButton.isVisible = false
+                        phoneLayout.error = "전화번호 형식이 다릅니다."
+                    }
                 }
             }
 
-            phoneEdit.setOnEditorActionListener(EditorInfo.IME_ACTION_DONE){
-                if(phoneEdit.length() > 9){
+            phoneEdit.setOnEditorActionListener(EditorInfo.IME_ACTION_DONE) {
+                if (phoneEdit.length() > 9) {
                     confirmButton.isVisible = true
                     phoneEdit.hideKeyboard()
                 }
             }
         }
+    }
+
+    private fun validName() = !binding.nameEdit.text.isNullOrBlank() && REGEX_NAME.toRegex()
+        .matches(binding.nameEdit.text!!)
+
+    private fun validBirthday() = !binding.birthdayEdit.text.isNullOrBlank() && REGEX_BIRTHDAY.toRegex()
+        .matches(binding.birthdayEdit.text!!)
+
+    private fun validPhone() = !binding.phoneEdit.text.isNullOrBlank() && REGEX_PHONE.toRegex()
+        .matches(binding.phoneEdit.text!!)
+
+    fun onClickDone() {
+        if (!validName()) {
+            binding.nameLayout.error = "1자 이상의 한글을 입력해 주세요"
+            return
+        }
+
+        if (!validBirthday()) {
+            binding.birthdayLayout.error = "생년월일 형식이 다릅니다."
+            return
+        }
+
+        if (!validPhone()) {
+            binding.phoneLayout.error = "전화번호 형식이 다릅니다."
+            return
+        }
+
+    }
+
+    companion object {
+        private const val REGEX_NAME = "^[가-힣]{2,}\$"
+        private const val REGEX_BIRTHDAY =
+            "^(19|20)[0-9]{2}(0[1-9]|1[0-2])(0[1-9]|[12][0-9]|3[01])$"
+        private const val REGEX_PHONE = "^01([016789])([0-9]{3,4})([0-9]{4})"
     }
 
 }
