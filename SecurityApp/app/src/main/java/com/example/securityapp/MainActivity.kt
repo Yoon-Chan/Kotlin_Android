12ec4a907d8627287e0221b1a157e057ea5b655e
윤찬 <vsvx13@naver.com>
2023-12-22 12:51:51 +0900

    Feat: 초기 화면(보안 키패드/휴대폰 인증화면 선택) 구현 및 셔플 키패드 구현

--- SecurityApp/app/src/main/java/com/example/securityapp/MainActivity.kt
+++ SecurityApp/app/src/main/java/com/example/securityapp/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.securityapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.securityapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.view = this
+    }
+
+    fun openShuffle() {
+
+    }
+
+    fun openVerifyOTP(){
+
     }
 }
