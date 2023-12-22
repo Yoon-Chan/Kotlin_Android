7c829c1e4d6abfa7ea30f7b4338b93c204fadd9a
윤찬 <vsvx13@naver.com>
2023-12-22 13:01:35 +0900

    Feat: 클릭 리스너 구현

--- SecurityApp/app/src/main/java/com/example/securityapp/widget/ShuffleNumberKeyboard.kt
+++ SecurityApp/app/src/main/java/com/example/securityapp/widget/ShuffleNumberKeyboard.kt
@@ -3,6 +3,7 @@ package com.example.securityapp.widget
 import android.content.Context
 import android.util.AttributeSet
 import android.view.LayoutInflater
+import android.view.View
 import android.widget.GridLayout
 import android.widget.TextView
 import androidx.core.view.children
@@ -13,14 +14,18 @@ class ShuffleNumberKeyboard @JvmOverloads constructor(
     context: Context,
     attributeSet: AttributeSet? = null,
     defStyle: Int = 0
-) : GridLayout(context, attributeSet, defStyle) {
+) : GridLayout(context, attributeSet, defStyle), View.OnClickListener {
 
     private var _binding: WidgetShuffleNumberKeyboardBinding? = null
     private val binding get() = _binding!!
 
+    private var listener: KeyPadListener? = null
+
     init {
         _binding =
             WidgetShuffleNumberKeyboardBinding.inflate(LayoutInflater.from(context), this, true)
+        binding.view = this
+        binding.listener = this
         shuffle()
     }
 
@@ -29,19 +34,43 @@ class ShuffleNumberKeyboard @JvmOverloads constructor(
         _binding = null
     }
 
-    private fun shuffle(){
+    private fun shuffle() {
         val keyNumberArray = ArrayList<String>()
-        for (i in 0..9){
+        for (i in 0..9) {
             keyNumberArray.add(i.toString())
         }
 
-        binding.gridLayout.children.forEach {view ->
-            if(view is TextView && view.tag != null){
+        binding.gridLayout.children.forEach { view ->
+            if (view is TextView && view.tag != null) {
                 val randIndex = Random.nextInt(keyNumberArray.size)
                 view.text = keyNumberArray[randIndex]
                 keyNumberArray.removeAt(randIndex)
             }
         }
+    }
+
+    fun setKeyPadListener(listener: KeyPadListener) {
+        this.listener = listener
+    }
+
+    fun onClickDelete() {
+        listener?.onClickDelete()
+    }
+
+    fun onClickDone() {
+        listener?.onClickDone()
+    }
+
+    override fun onClick(v: View?) {
+        if (v is TextView && v.tag != null) {
+            listener?.onClickNumber(v.text.toString())
+        }
+    }
+
+    interface KeyPadListener {
+        fun onClickNumber(num: String)
+        fun onClickDelete()
+        fun onClickDone()
     }
 
 }
